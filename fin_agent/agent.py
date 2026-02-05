6d93ee70717e8399218343dd0cb386bc1c153e2a
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-05 20:00:56 +0530

    feat:added the ability to do google search

--- fin_agent/agent.py
+++ fin_agent/agent.py
@@ -1,15 +1,18 @@
 from dotenv import load_dotenv
 import os
-from /home/dhanush/Desktop/agentic_ai/invesment_plan/agent.py import investment_agent
+#from /home/dhanush/Desktop/agentic_ai/invesment_plan/agent.py import investment_agent
 load_dotenv()
 from google.adk.agents import LlmAgent
+from google.adk.tools import google_search
 finance_agent=LlmAgent(
      name='finance_agent_analyser',
      description="Helps user with finance related questions",
      model='gemini-2.5-flash',
      instruction='''1)Take the finance query from the user
      2)Answer the quiestion with required  answer 
-     '''
+     ''',
+      
+     tools=[google_search]
 
     )
 root_agent=finance_agent
